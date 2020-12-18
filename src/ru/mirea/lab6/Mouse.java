9ad150908f917412a96faebe579dee10ff8a9898
cCraBb1538 <dyukoivan@gmail.com>
2020-12-18 15:52:00 +0300

    Initial commit

---
+++ src/ru/mirea/lab6/Mouse.java
@@ -0,0 +1,30 @@
+package ru.mirea.lab6;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class MyMouse extends JFrame
+{
+    JLabel lbl = new JLabel("");
+    public MyMouse()
+    {
+        super("Where's my mouse ?");
+        setSize(400,400);
+        setLayout(new BorderLayout());
+        add(lbl,BorderLayout.SOUTH);
+        addMouseListener(new MouseListener()
+        {
+            public void mouseExited(MouseEvent a){}
+            public void mouseClicked(MouseEvent a) {lbl.setText("X="+a.getX()+" Y="+a.getY());}
+            public void mouseEntered(MouseEvent a) {}
+            public void mouseReleased(MouseEvent a) {}
+            public void mousePressed(MouseEvent a) {}
+
+        });
+    }
+    public static void main(String[]args)
+    {
+        new MyMouse().setVisible(true);
+    }
+}
